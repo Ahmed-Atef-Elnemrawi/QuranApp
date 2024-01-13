be4d3d4bc92c585d4276807d2e64706a9d7acfb4
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2024-01-13 18:03:17 +0200

    "feat(app): Add AudioService class for managing audio playback
    This service class encapsulates audio playback functionality, providing methods
    to play, pause, stop, seek, change volume, mute, and observe audio state. It uses
    the AudioStateManager to manage the state of the underlying HTML audio element."

---
+++ src/app/core/audio.service.ts
@@ -0,0 +1,92 @@
+import { Injectable, Renderer2, inject } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { AudioState, AudioStateManager } from './audio-state-manager';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AudioService {
+  #renderer = inject(Renderer2);
+  #audio = new Audio();
+  #stateManager = new AudioStateManager(this.#audio);
+  #stop$ = new Subject<void>();
+  #audioEvents = [
+    'ended',
+    'error',
+    'play',
+    'playing',
+    'pause',
+    'timeupdate',
+    'canplay',
+    'loadedmetadata',
+    'loadstart',
+  ];
+
+  playStream(url: string): Observable<Event> {
+    return this.streamObservable(url).pipe(takeUntil(this.#stop$));
+  }
+
+  play(): void {
+    this.#audio.play();
+  }
+
+  pause(): void {
+    this.#audio.pause();
+  }
+
+  stop(): void {
+    this.#stop$.next();
+  }
+
+  seekTo(seconds: number): void {
+    this.#audio.currentTime = seconds;
+  }
+
+  changeVolume(value: number): void {
+    this.#audio.volume = value / 100;
+  }
+
+  mute(value: boolean): void {
+    this.#audio.muted = value;
+  }
+
+  getState(): Observable<AudioState> {
+    return this.#stateManager.state$;
+  }
+
+  private streamObservable(url: string): Observable<Event> {
+    return new Observable((subscriber) => {
+      this.setupAudioEl(url);
+      const eventHandler = (event: Event) => {
+        this.#stateManager.updateState(event);
+        subscriber.next(event);
+      };
+
+      this.addEvents(this.#audio, this.#audioEvents, eventHandler);
+
+      return () => this.cleanupAudioEl();
+    });
+  }
+
+  private setupAudioEl(url: string): void {
+    this.#audio.src = url;
+    this.#audio.load();
+    this.#audio.play();
+  }
+
+  private cleanupAudioEl(): void {
+    this.#audio.pause();
+    this.#audio.currentTime = 0;
+    this.#stateManager.resetState();
+  }
+
+  private addEvents(
+    obj: HTMLAudioElement,
+    events: string[],
+    handler: (event: Event) => void
+  ): void {
+    events.forEach((event) => {
+      this.#renderer.listen(obj, event, handler);
+    });
+  }
+}
