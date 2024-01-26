684bd551c5ada6a977bb23f1d816a4de111954c7
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2024-01-26 14:23:14 +0200

    feat: Introduce Operations<T> interface
    Defines a concise Operations<T> interface for basic CRUD operations on an IndexedDB object store. This interface enhances clarity and maintainability in the AppIDBService.
    
    # Operations<T> Interface
    - `get`, `getAll`, `add`, `update`, `delete`, `clear`
    
    # AppIDBService
    - Uses Operations<T> for encapsulating CRUD operations.

---
+++ src/app/core/DB/operations.ts
@@ -0,0 +1,41 @@
+export interface Operations<T> {
+  /**
+   * Gets an item from the object store by key.
+   * @param {string} key The key of the item to get.
+   * @returns {Promise<T>} A promise that resolves to the item.
+   */
+  get: (key: number | string) => Promise<T>;
+
+  /**
+   * Gets all items from the object store.
+   * @returns {Promise<T[]>} A promise that resolves to an array of items.
+   */
+  getAll: () => Promise<T[]>;
+
+  /**
+   * Adds an item to the object store.
+   * @param {T} item The item to add.
+   * @returns {Promise<void>} A promise that resolves when the item is added.
+   */
+  add: (item: T) => Promise<void>;
+
+  /**
+   * Updates an item in the object store.
+   * @param {T} item The item to update.
+   * @returns {Promise<void>} A promise that resolves when the item is updated.
+   */
+  update: (item: T) => Promise<void>;
+
+  /**
+   * Deletes an item from the object store by ID.
+   * @param {number} id The ID of the item to delete.
+   * @returns {Promise<void>} A promise that resolves when the item is deleted.
+   */
+  delete: (key: string) => Promise<void>;
+
+  /**
+   * Clears the object store.
+   * @returns {Promise<void>} A promise that resolves when the store is cleared.
+   */
+  clear:() => Promise<void>;
+}
